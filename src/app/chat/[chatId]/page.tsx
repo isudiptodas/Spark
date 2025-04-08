46fbf8278b8fef76b476c98bce1aeaa7bc0d8e7e
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-04-09 00:33:01 +0530

    Update main chat window

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -6,8 +6,13 @@ export default async function Page({ params, }: {
 
     return (
         <>
+            <div className="w-full h-screen flex flex-col justify-start overflow-hidden items-center gap-3 bg-black relative">
+                <div className="h-56 w-56 lg:h-72 lg:w-[500px] absolute z-10 -top-24 -right-20 lg:-top-56 lg:-right-16 opacity-40 rounded-full bg-orange-400 blur-[120px]"></div>
 
-            <p>chat window - {question}</p>
+                <h1 className=" font-black z-30 text-2xl md:py-10 py-5 bg-gradient-to-br from-orange-300 via-orange-700 to-transparent bg-clip-text text-transparent">SPARK</h1>
 
-        </>)
+
+            </div>
+        </>
+    )
 }
