d74ec018f0c8e7073484305bf5fb79899f493363
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-04-10 21:17:14 +0530

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,11 +4,6 @@ export const connectDB = async () => {
     try{
         const URI = process.env.MONGODB_URI;
 
-        if(!URI){
-            console.error('URI is not defined');
-            throw new Error('Connection not defined');
-        }
-
         await mongoose.connect(URI);
         console.log('Databse Connected');
     }
