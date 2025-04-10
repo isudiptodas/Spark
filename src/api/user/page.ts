c58641818123d0f8848f58fb074ed6632b951b7d
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-04-10 18:41:56 +0530

    Delete src/api/user directory

--- src/api/user/page.ts
+++
@@ -1,6 +0,0 @@
-import { connectDB } from "@/config/db";
-import { NextRequest, NextResponse } from "next/server";
-
-export const userHandler = async (req: NextRequest, res: NextResponse) => {
-    await connectDB();
-}
