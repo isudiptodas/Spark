c6ea2f55971f2757c04efde9fd529beb905a7f60
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-04-12 02:34:22 +0530

    added gemini response

--- src/app/api/gemini/route.ts
+++ src/app/api/gemini/route.ts
@@ -1,39 +1,84 @@
 import { GoogleGenerativeAI } from '@google/generative-ai'
 import { Task } from '@/models/taskModel';
-import { NextRequest } from 'next/server';
-import mime from 'mime-types';
+import { NextRequest, NextResponse } from 'next/server';
+import { connectDB } from "@/config/db";
 
 export async function POST(req: NextRequest) {
+
+    await connectDB();
 
     const { prompt } = await req.json();
     const userId = req.headers.get('userId');
 
-    const newChat = {
-        role: 'user',
-        message: prompt
-    };
+    try {
+        const newChat = {
+            role: 'user',
+            message: prompt
+        };
 
-    const api = process.env.GEMINI_API as string;
-    const genAI = new GoogleGenerativeAI(api);
+        //console.log(prompt);
 
-    const model = genAI.getGenerativeModel({
-        model: "gemini-1.5-flash",
-    });
+        const api = process.env.GEMINI_API as string;
+        const genAI = new GoogleGenerativeAI(api);
 
-    const generationConfig = {
-        temperature: 1,
-        topP: 0.95,
-        topK: 40,
-        maxOutputTokens: 8192,
-        responseModalities: [
-        ],
-        responseMimeType: "text/plain",
-    };
+        const model = genAI.getGenerativeModel({
+            model: "gemini-1.5-flash",
+        });
 
-    const chatSession = model.startChat({
-        generationConfig,
-        history: [
-            
-        ],
-      });
+        const generationConfig = {
+            temperature: 1,
+            topP: 0.95,
+            topK: 40,
+            maxOutputTokens: 8192,
+            responseModalities: [
+            ],
+            responseMimeType: "application/json",
+        };
+
+        const chatSession = model.startChat({
+            generationConfig,
+            history: [
+            ],
+        });
+
+        const add = process.env.TASK_PROMPT as string;
+
+        const result = await chatSession.sendMessage(`${prompt}${add}`);
+
+        const resp = result.response.text();
+       // const parsed = JSON.parse(resp);
+       // console.log(parsed);
+
+        const parsed = JSON.parse(resp);
+        //console.log(parsed);
+
+        const newTask = new Task({
+            task: parsed.taskName,
+            actual: parsed.actual,
+            taskHistory: [
+                { role: 'user', message: prompt },
+                { role: 'gemini', message: parsed.taskResponse }
+            ],
+            taskExplanation: parsed.explanation
+            //userId: userId
+        });
+
+        await newTask.save();
+
+        //console.log("done");
+
+        return NextResponse.json({
+            status: 200,
+            success: true,
+            message: 'Success',
+            newTask
+        });
+
+    } catch (err) {
+        console.log(err);
+        return NextResponse.json({
+            status: 500,
+            success: false,
+        });
+    }
 }
