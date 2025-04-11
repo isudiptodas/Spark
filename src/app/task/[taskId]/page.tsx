06ae4540358b33a94169b1194b5adf7e9455ad0a
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-04-12 02:34:55 +0530

    displayed gemini response

--- src/app/task/[taskId]/page.tsx
+++ src/app/task/[taskId]/page.tsx
@@ -7,6 +7,18 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { use } from "react";
 import { Toaster, toast } from "sonner";
 import axios from "axios";
+import Markdown from 'react-markdown'
+
+interface data {
+    task : string,
+    taskExplanation: [{}],
+    actual: string
+}
+
+interface convo {
+    role: string,
+    message: string
+}
 
 export default function Page({ params, }: { params: Promise<{ taskId: string }> }) {
 
@@ -17,26 +29,32 @@ export default function Page({ params, }: { params: Promise<{ taskId: string }> 
     const searchParams = useSearchParams();
     const [loaded, setLoaded] = useState(false);
     const [generating, setGenerating] = useState(false);
+    const[data, setData] = useState<data>();
+    const[conversation, setConversation] = useState<convo[]>([]);
 
     const generate = async () => {
-        if (!prompt) {
-            toast.error("Please enter prompt");
-            return;
-        }
 
         try {
             setGenerating(true);
             const res = await axios.post('/api/gemini', {
-                prompt
+                prompt: question
             });
 
-            console.log(res.data);
+            setData(res.data.newTask);
+            //const parsed = JSON.parse(res.data.response);
+           // console.log(parsed);
         } catch (err) {
             console.log(err);
+        }
+        finally{
+            setGenerating(false);
         }
     }
 
     useEffect(() => {
+        if(question){
+            generate();
+        }
         setTimeout(() => {
             setLoaded(true);
         }, 2000);
@@ -53,17 +71,21 @@ export default function Page({ params, }: { params: Promise<{ taskId: string }> 
 
                 <div className="w-full h-full px-2 py-3 lg:py-0 lg:-mt-5 flex flex-col lg:flex-row-reverse justify-between items-center lg:items-start gap-3 overflow-hidden scrollbar-hide">
 
-                    <div className="flex bg-gradient-to-b from-zinc-950 via-zinc-950 to-transparent h-[73%] overflow-y-auto lg:h-[77vh] rounded-md w-full lg:w-[70%] flex-col justify-between items-center gap-3 px-3 py-2 relative">
+                    <div className="flex text-[12px] lg:text-sm px-5 py-5 bg-gradient-to-b text-white from-zinc-950 via-zinc-950 to-transparent h-[73%] overflow-y-auto lg:h-[77vh] rounded-md w-full lg:w-[70%] flex-col justify-start items-center gap-3 relative">
                         <div className={`absolute z-50 bg-black opacity-80 top-0 rounded-md h-full w-full ${generating ? "block" : "hidden"} flex justify-center items-center text-white`}>Generating your response...</div>
-
+                        <Markdown>
+                            {data?.actual}
+                        </Markdown>
                     </div>
 
                     <div className=" flex bg-gradient-to-b from-zinc-950 via-zinc-950 to-transparent h-96 lg:h-[77vh] rounded-md w-full lg:w-[30%] flex-col justify-start lg:justify-between items-center gap-3 px-3 py-2 relative">
                         <div className={`absolute z-50 bg-black opacity-80 top-0 rounded-md h-full w-full ${generating ? "block" : "hidden"} flex justify-center items-center text-white`}>Generating your response...</div>
                         <Link href='/new-chat' className="hidden lg:block w-full text-center py-2 rounded-md bg-white text-black cursor-pointer hover:opacity-70 duration-200 ease-in-out active:scale-95">New Chat +</Link>
 
-                        <div className="w-full h-full bg-zinc-900 rounded-md ">
-
+                        <div className="w-full h-full bg-zinc-900 rounded-md flex flex-col justify-start items-center gap-2">
+                            {/* {conversation.length > 0 && conversation.map((convo, index) => {
+                                return <div key={index} className={`w-full py-2 px-2 flex ${convo?.role === 'user' ? "justify-end bg-orange-400 text-white" : "justify-start bg-gradient-to-r from-blue-400 to-purple-400 text-white"}`}>{convo?.message}</div>
+                            })} */}
                         </div>
 
                         <textarea onChange={(e) => setPrompt(e.target.value)} className="w-full px-2 py-2 outline-none h-14 lg:h-24 bg-zinc-800 rounded-md text-white" placeholder="Ask your question" />
