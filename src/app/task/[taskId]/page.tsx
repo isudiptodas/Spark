eb14970ad9d52af9263bae68a94250c93adc98a0
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-04-12 22:06:47 +0530

    displayed recent gemini chats

--- src/app/task/[taskId]/page.tsx
+++ src/app/task/[taskId]/page.tsx
@@ -25,12 +25,14 @@ export default function Page({ params, }: { params: Promise<{ taskId: string }> 
     const { taskId } = use(params);
     const question = decodeURIComponent(taskId);
     const [prompt, setPrompt] = useState('');
+    const[actual, setActual] = useState('');
     const router = useRouter();
     const searchParams = useSearchParams();
     const [loaded, setLoaded] = useState(false);
     const [generating, setGenerating] = useState(false);
     const[data, setData] = useState<data>();
-    const[conversation, setConversation] = useState<convo[]>([]);
+    const[convo, setConvo] = useState<convo[]>([]);
+    const[chatId, setChatId] = useState('');
 
     const generate = async () => {
 
@@ -41,6 +43,36 @@ export default function Page({ params, }: { params: Promise<{ taskId: string }> 
             });
 
             setData(res.data.newTask);
+            //console.log(res.data.newTask);
+            setConvo(res.data.newTask.taskExplanation);
+            //const parsed = JSON.parse(res.data.response);
+           // console.log(parsed);
+           setChatId(res.data.newTask._id);
+           const text = JSON.stringify(res.data.newTask.actual);
+           setActual(text);
+        } catch (err) {
+            console.log(err);
+        }
+        finally{
+            setGenerating(false);
+        }
+    }
+
+    const answer = async () => {
+
+        try {
+            setGenerating(true);
+            const res = await axios.put('/api/gemini', {
+                prompt, id: chatId
+            });
+
+            setPrompt('');
+
+            setData(res.data.newTask);
+            //console.log(res.data.newTask);
+            setConvo(res.data.newTask.taskExplanation);
+            const text = JSON.stringify(res.data.newTask.actual);
+           setActual(text);
             //const parsed = JSON.parse(res.data.response);
            // console.log(parsed);
         } catch (err) {
@@ -52,9 +84,7 @@ export default function Page({ params, }: { params: Promise<{ taskId: string }> 
     }
 
     useEffect(() => {
-        if(question){
-            generate();
-        }
+        generate();
         setTimeout(() => {
             setLoaded(true);
         }, 2000);
@@ -72,24 +102,25 @@ export default function Page({ params, }: { params: Promise<{ taskId: string }> 
                 <div className="w-full h-full px-2 py-3 lg:py-0 lg:-mt-5 flex flex-col lg:flex-row-reverse justify-between items-center lg:items-start gap-3 overflow-hidden scrollbar-hide">
 
                     <div className="flex text-[12px] lg:text-sm px-5 py-5 bg-gradient-to-b text-white from-zinc-950 via-zinc-950 to-transparent h-[73%] overflow-y-auto lg:h-[77vh] rounded-md w-full lg:w-[70%] flex-col justify-start items-center gap-3 relative">
-                        <div className={`absolute z-50 bg-black opacity-80 top-0 rounded-md h-full w-full ${generating ? "block" : "hidden"} flex justify-center items-center text-white`}>Generating your response...</div>
+                        <div className={`absolute z-50 bg-black opacity-80 top-0 rounded-md h-full w-full ${generating ? "block" : "hidden"} flex justify-center items-center text-white`}>Refreshing your response...</div>
                         <Markdown>
-                            {data?.actual}
+                            {/* {data?.actual} */}
+                            {actual}
                         </Markdown>
                     </div>
 
                     <div className=" flex bg-gradient-to-b from-zinc-950 via-zinc-950 to-transparent h-96 lg:h-[77vh] rounded-md w-full lg:w-[30%] flex-col justify-start lg:justify-between items-center gap-3 px-3 py-2 relative">
-                        <div className={`absolute z-50 bg-black opacity-80 top-0 rounded-md h-full w-full ${generating ? "block" : "hidden"} flex justify-center items-center text-white`}>Generating your response...</div>
+                        <div className={`absolute z-50 bg-black opacity-80 top-0 rounded-md h-full w-full ${generating ? "block" : "hidden"} flex justify-center items-center text-white`}>Answering your question...</div>
                         <Link href='/new-chat' className="hidden lg:block w-full text-center py-2 rounded-md bg-white text-black cursor-pointer hover:opacity-70 duration-200 ease-in-out active:scale-95">New Chat +</Link>
 
-                        <div className="w-full h-full bg-zinc-900 rounded-md flex flex-col justify-start items-center gap-2">
-                            {/* {conversation.length > 0 && conversation.map((convo, index) => {
-                                return <div key={index} className={`w-full py-2 px-2 flex ${convo?.role === 'user' ? "justify-end bg-orange-400 text-white" : "justify-start bg-gradient-to-r from-blue-400 to-purple-400 text-white"}`}>{convo?.message}</div>
-                            })} */}
+                        <div className="w-full h-full bg-zinc-900 rounded-md flex flex-col justify-start items-center gap-2 px-3 py-2 overflow-y-auto">
+                            {convo?.length > 0 && convo?.map((convo, index) => {
+                                return <div key={index} className={`w-full py-2 text-[12px] px-2 flex ${convo?.role === 'user' ? "justify-end bg-transparent text-white rounded-md" : "justify-start bg-orange-400 rounded-md text-white"}`}>{convo?.message}</div>
+                            })}
                         </div>
 
-                        <textarea onChange={(e) => setPrompt(e.target.value)} className="w-full px-2 py-2 outline-none h-14 lg:h-24 bg-zinc-800 rounded-md text-white" placeholder="Ask your question" />
-                        <span className={`p-2 ${prompt === '' ? "hidden" : "block"} rounded-md cursor-pointer text-sm bottom-5 right-5 duration-200 ease-in-out active:scale-95 absolute bg-orange-400 text-white`}><IoSparklesSharp /></span>
+                        <textarea onChange={(e) => setPrompt(e.target.value)} className="w-full px-2 py-2 outline-none h-14 lg:h-24 bg-zinc-800 rounded-md text-white" placeholder="Ask your question" value={prompt}/>
+                        <span className={`p-2 ${prompt === '' ? "hidden" : "block"} rounded-md cursor-pointer text-sm bottom-5 right-5 duration-200 ease-in-out active:scale-95 absolute bg-orange-400 text-white`} onClick={answer}><IoSparklesSharp /></span>
                     </div>
                 </div>
             </div>
