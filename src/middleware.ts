a7f179bb6078c5f920804a98b77593affec271ea
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-04-13 01:21:16 +0530

    added middleware in task and history page

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,5 +26,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/new-chat', '/chat/:path*']
+    matcher: ['/new-chat', '/chat/:path*', '/history/:path*', '/task/:path*']
 }
