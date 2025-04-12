e8fe4a3205df099f222d87d34e31bb65518347f7
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-04-12 22:07:59 +0530

    Updated task model

--- src/models/taskModel.ts
+++ src/models/taskModel.ts
@@ -8,8 +8,9 @@ const explanationSchema = new mongoose.Schema({
 const taskSchema = new mongoose.Schema({
     task: {type: String},
     category: {type: String, default: "task"},
-    taskExplanation: {type: String}, 
+    taskExplanation:  {type: [explanationSchema]}, 
     actual: {type: String}, 
+    initialPrompt: {type: String}, 
     userId: { type: Schema.Types.ObjectId, ref: 'User'},
     taskHistory: {type: [explanationSchema]}
 }, {timestamps: true});
