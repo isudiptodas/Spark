375a32d305fd4903a070e7c346ed25abca096de4
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-04-13 19:07:45 +0530

    added API calling code

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -12,22 +12,54 @@ import Link from "next/link";
 import { IoSparklesSharp } from "react-icons/io5";
 import { useRouter, useSearchParams } from "next/navigation";
 import { use } from "react";
+import axios from "axios";
 
 export default function Page({ params, }: { params: Promise<{ chatId: string }> }) {
 
-    const { chatId } =  use(params);
+    interface convo {
+        role: string,
+        message: string
+    }
+
+    const { chatId } = use(params);
     const question = decodeURIComponent(chatId);
     const [prompt, setPrompt] = useState('');
     const [option, setOption] = useState('preview');
+    const [generatedFiles, setGeneratedFiles] = useState();
     const router = useRouter();
     const searchParams = useSearchParams();
+    const [generating, setGenerating] = useState(false);
+    const [conversation, setConversation] = useState<convo[]>([]);
     const template = searchParams.get('template');
-    const[loaded, setLoaded] = useState(false);
+    const [loaded, setLoaded] = useState(false);
 
-    useEffect(()=> {
-       setTimeout(()=> {
-        setLoaded(true);
-       }, 2000);
+    const generate = async () => {
+        try {
+            setGenerating(true);
+            const res = await axios.post(`/api/gemini-files`, {
+                prompt: question, template
+            });
+
+            //console.log(res.data);
+            // const converted = res.data.newFile;
+            // const file = JSON.parse(converted);
+
+            //console.log(file);
+            //setConversation(res.data.newTask.chat);
+            //setGeneratedFiles(file);
+        } catch (err) {
+            console.log(err);
+        }
+        finally {
+            setGenerating(false);
+        }
+    }
+
+    useEffect(() => {
+        generate();
+        setTimeout(() => {
+            setLoaded(true);
+        }, 2000);
     }, []);
 
     return (
@@ -39,13 +71,23 @@ export default function Page({ params, }: { params: Promise<{ chatId: string }> 
 
                 <div className="w-full h-full px-2 py-3 lg:py-0 lg:-mt-5 flex flex-col lg:flex-row justify-start items-center lg:items-start gap-3 overflow-hidden scrollbar-hide">
 
-                    <div className="hidden lg:flex bg-gradient-to-b from-zinc-950 via-zinc-950 to-transparent h-[77vh] rounded-md w-[30%] flex-col justify-between items-center gap-3 px-3 py-2 relative">
+                    <div className=" flex bg-gradient-to-b from-zinc-950 via-zinc-950 to-transparent h-96 lg:h-[77vh] rounded-md w-full lg:w-[30%] flex-col justify-start lg:justify-between items-center gap-3 px-3 py-2 relative">
+                        <div className={`absolute z-50 bg-black opacity-80 top-0 rounded-md h-full w-full ${generating ? "block" : "hidden"} flex justify-center items-center text-white`}>Answering your question...</div>
+
                         <Link href='/new-chat' className="w-full text-center py-2 rounded-md bg-white text-black cursor-pointer hover:opacity-70 duration-200 ease-in-out active:scale-95">New Chat +</Link>
+                        <div className="w-full h-full bg-zinc-900 rounded-md flex flex-col justify-start items-center gap-2 px-3 py-2 overflow-y-auto">
+                            {conversation?.length > 0 && conversation?.map((convo, index) => {
+                                return <div key={index} className={`w-full py-2 text-[12px] px-2 flex ${convo?.role === 'user' ? "justify-end bg-transparent text-white rounded-md" : "justify-start bg-orange-400 rounded-md text-white"}`}>{convo?.message}</div>
+                            })}
+                        </div>
                         <textarea onChange={(e) => setPrompt(e.target.value)} className="w-full px-2 py-2 outline-none h-24 bg-zinc-800 rounded-md text-white" placeholder="Ask your question" />
                         <span className={`p-2 ${prompt === '' ? "hidden" : "block"} rounded-md cursor-pointer text-sm bottom-5 right-5 duration-200 ease-in-out active:scale-95 absolute bg-orange-400 text-white`}><IoSparklesSharp /></span>
                     </div>
 
                     <div className="w-full h-full flex flex-col z-50 lg:pr-4 scrollbar-hide">
+
+                        <div className={`absolute z-50 bg-black opacity-80 top-0 rounded-md h-full w-full ${generating ? "block" : "hidden"} flex justify-center items-center text-white`}>Refreshing your response...</div>
+
 
                         <div className={`w-full flex justify-start items-start gap-3 px-4 py-3 rounded-md bg-zinc-950`}>
                             <span onClick={() => setOption('chat')} className={` text-[12px] md:text-sm px-3 py-1 rounded-full lg:hidden cursor-pointer duration-200 ease-in-out border-[1px] ${option === 'chat' ? "border-2 border-emerald-600 bg-emerald-300 text-black" : "text-white border-zinc-700 bg-black"}`}>Chat</span>
@@ -61,7 +103,7 @@ export default function Page({ params, }: { params: Promise<{ chatId: string }> 
                         </div>}
 
                         <div className={` w-full h-full scrollbar-hide ${option === 'chat' ? "hidden" : "block"}`}>
-                            <SandpackProvider theme={'dark'}>
+                            <SandpackProvider files={generatedFiles} theme={'dark'}>
                                 <SandpackLayout className="h-[80vh] lg:h-[70vh] w-full lex flex-col scrollbar-hide">
                                     {option === 'code' && <SandpackCodeEditor className={`h-full w-full`} showInlineErrors showTabs closableTabs showLineNumbers={true} />}
                                     {option === 'preview' && <SandpackPreview className={`h-full w-full`} />}
@@ -81,18 +123,18 @@ export default function Page({ params, }: { params: Promise<{ chatId: string }> 
             </div>
 
             <div className={`${loaded ? "hidden" : "block"} overflow-hidden px-4 h-screen w-full flex flex-col lg:flex-row justify-start lg:justify-center py-4 items-center gap-5 bg-black`}>
-              <div className="lg:hidden w-full h-auto flex flex-col justify-start items-center gap-3">
-                 <div className="bg-zinc-900 w-full h-96 rounded-md lg:rounded-lg"></div>
-                 <div className="bg-zinc-900 w-full h-10 rounded-md lg:rounded-lg"></div>
-                 <div className="bg-zinc-900 w-full h-64 rounded-md lg:rounded-lg"></div>
-                 <div className="bg-zinc-900 w-full h-32 rounded-md lg:rounded-lg"></div>
-                 <div className="bg-zinc-900 w-full h-32 rounded-md lg:rounded-lg"></div>
-              </div>
+                <div className="lg:hidden w-full h-auto flex flex-col justify-start items-center gap-3">
+                    <div className="bg-zinc-900 w-full h-96 rounded-md lg:rounded-lg"></div>
+                    <div className="bg-zinc-900 w-full h-10 rounded-md lg:rounded-lg"></div>
+                    <div className="bg-zinc-900 w-full h-64 rounded-md lg:rounded-lg"></div>
+                    <div className="bg-zinc-900 w-full h-32 rounded-md lg:rounded-lg"></div>
+                    <div className="bg-zinc-900 w-full h-32 rounded-md lg:rounded-lg"></div>
+                </div>
 
-              <div className="hidden lg:flex w-full h-auto flex-row justify-start items-center gap-3">
-                 <div className="bg-zinc-900 w-[30%] h-[90vh] rounded-md lg:rounded-lg"></div>
-                 <div className="bg-zinc-900 w-[70%] h-[90vh] rounded-md lg:rounded-lg"></div>
-              </div>
+                <div className="hidden lg:flex w-full h-auto flex-row justify-start items-center gap-3">
+                    <div className="bg-zinc-900 w-[30%] h-[90vh] rounded-md lg:rounded-lg"></div>
+                    <div className="bg-zinc-900 w-[70%] h-[90vh] rounded-md lg:rounded-lg"></div>
+                </div>
             </div>
         </>
     )
